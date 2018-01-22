917d8ca135b11987c8c07c6132c22c6ffeba2dda
Holly Hendry <hrh517@york.ac.uk>
2018-01-22 11:10:43 +0000

    More tree examples and resized pop for 30

--- snakeProblem30.py
+++ snakeProblem30.py
@@ -24,7 +24,7 @@ GRIDSIZE = YSIZE*XSIZE
 NFOOD = 1 # TODO: CHECK THAT THERE ARE ENOUGH SPACES LEFT FOR THE FOOD (IF THE TAIL IS VERY LONG)
 TOTALFOOD = 185 # total possible amount of food that can be eaten 
 NGEN = 500 # number of generations
-NPOP = 5000 # size of the population
+NPOP = 1000 # size of the population
 maxDepth = 17 # depth of the decision tree
 CXPB = 0.8 # probability of mating
 MUTX = 0.5 # probability of mutation
@@ -281,19 +281,35 @@ class SnakePlayer(list):
 	def ifFoodLeft(self, out1, out2):
 		return partial(if_then_else, self.senseFoodLeft, out1, out2)
 
+def emptySpaces(snake):
+	spaces = []
+	for i in range(1, (YSIZE-2)):
+		for j in range(1, (XSIZE-2)):
+			if not [i,j] in snake.body:
+				spaces.append([i,j])
+	return spaces
+
 # This function places a food item in the environment
 def placeFood(snake):
 	food = []
 	if (GRIDSIZE) == len(snake.body):
 		return None
-	timer = 0
-	while len(food) < NFOOD and timer < GRIDSIZE+1:
-		potentialfood = [random.randint(1, (YSIZE-2)), random.randint(1, (XSIZE-2))]
-		if not (potentialfood in snake.body) and not (potentialfood in food):
-			food.append(potentialfood)
-		timer += 1
-	if timer == GRIDSIZE:
+	#timer = 0
+	#while len(food) < NFOOD and timer < GRIDSIZE+1:
+	#	potentialfood = [random.randint(1, (YSIZE-2)), random.randint(1, (XSIZE-2))]
+	#	if not (potentialfood in snake.body) and not (potentialfood in food):
+	#		food.append(potentialfood)
+	#	timer += 1
+	#if timer == GRIDSIZE:
+	#	return None
+	spaces = emptySpaces(snake)
+	if len(spaces) == 0:
 		return None
+	while len(food) < NFOOD:
+		pos = random.randint(1, len(spaces))
+		potentialfood = spaces[pos-1]
+		if not (potentialfood in snake.body) and not (potentialfood in food):
+			food.append(potentialfood)	
 	snake.food = food  # let the snake know where the food is
 	return(food)
 
